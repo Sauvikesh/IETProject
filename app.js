8c496fb20b3c2713791a5ad76256008a16a59152
Sauvikesh <81947858+Sauvikesh@users.noreply.github.com>
2023-10-29 14:43:33 -0700

    feat: defined /posts endpoint, and returned activities

--- app.js
+++ app.js
@@ -1,4 +1,33 @@
 const express = require('express');
 const axios = require('axios');
 
+const port = 8080;
+const app = express();
 
+// define url and response to posts
+app.get("/posts", (req, res) => {
+    axios.get("https://aggiefeed.ucdavis.edu/api/v1/activity/public?s=0&l=10")
+    .then((response) => {
+        // extracts data 
+        const dataObtained = response.data;
+
+        // iterates through each object and extracts the wanted information
+        const activities = dataObtained.map((activity) => ({
+            id: activity.id,
+            published: activity.published,
+            title: activity.title,
+            actorDisplayName: activity.actor.displayName,
+          }));
+      
+          // sends the requested number of activities
+          res.send(activities);
+      })
+      .catch((error) => {
+        // sends an error if there was an error
+        console.error(error);
+      });
+});
+
+app.listen(port,  () => {
+    console.log("Server listening on", port);
+});
