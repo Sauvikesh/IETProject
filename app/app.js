b3e5636e421f13c8a8108a00c12dea12e1da6442
Sauvikesh <81947858+Sauvikesh@users.noreply.github.com>
2023-12-13 13:42:05 -0800

    AF-90 : get request to RSS, made frontend middleware

--- app/app.js
+++ app/app.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const axios = require('axios');
 const mongoose = require('mongoose');
 const port = 3000;
 const app = express();
 
+// Frontend route
+const FrontRouter = require('./routes/front');
 
 /*
     required data for RSS:
@@ -24,36 +25,8 @@ mongoose
     .catch(error => console.log(error));
 
 
-// Home page route
-app.get('/', async (req, res) => {
-    res.send("this is the home page");
-});
+app.use(FrontRouter);
 
-// define url and response to /posts
-app.get("/posts", (req, res) => {
-    axios.get("https://aggiefeed.ucdavis.edu/api/v1/activity/public?s=0&l=10")
-    .then((response) => {
-        // extracts data from response
-        const dataObtained = response.data;
-
-        // iterates through each object and extracts desired information 
-        const activities = dataObtained.map((activity) => ({
-            id: activity.id,
-            published: activity.published,
-            title: activity.title,
-            author: activity.actor.displayName,
-            description: activity.object.content,
-            link: activity.object.ucdEdusModel.url
-          }));
-      
-          // sends the requested number of activities in an array
-          res.send(activities);
-      })
-      .catch((error) => {
-        // sends an error if there was an error
-        console.error(error);
-      });
-});
 
 app.listen(port, () => {
     console.log("Server listening on", port);
