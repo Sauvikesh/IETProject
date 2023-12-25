f8938b4c4d322cb1668318897e9964b04fdafaa5
Sauvikesh <81947858+Sauvikesh@users.noreply.github.com>
2023-12-24 22:16:30 -0800

    AF-90: processor now returns an array of activity objects

---
+++ app/models/AFactivity.js
@@ -0,0 +1,81 @@
+const mongoose = require('mongoose');
+
+// Schema for activity
+// UNFINISHED, need to add some more data and need to add which fields are required
+const activity = new mongoose.Schema( {
+    icon: String,
+    actor: {
+        id: String,
+        objectType: String,
+        displayName: String,
+        image: {
+            color: String
+        },
+        author: {
+            id: String,
+            displayName: String
+        },
+    },
+    verb: String,
+    title: String,
+    object: {
+        ucdSrcId: String,
+        objectType: String,
+        content: String,
+        contentImage: {
+            dimensions: {
+                normal: {
+                    url: String,
+                    width: String,
+                    height: String,
+                },
+                high: {
+                    url: String,
+                    width: String,
+                    height: String,
+                }
+            },
+            source: String,
+
+        },
+        ucdEdusModel: {
+            url: String,
+            urlDisplayName: String,
+            event : {
+                hasStartTime: Boolean,
+                hasEndTime: Boolean,
+                location: String,
+                startDate: String,
+                endDate: String,
+                isAllDay: Boolean,
+            }
+        },
+        location: {
+            displayName: String,
+            geo: {
+                latitidue: String,
+                longitiude: String,
+            },
+            geometry: {
+                type: String,
+                coordinates: Array
+            }
+        },
+    },
+    generator: {
+        id: String
+    },
+    to: Array,
+    published: String,
+    ucdEdusMeta: {
+        labels: Array,
+        startDate: String,
+        endDate: String
+    },
+
+ });
+
+
+const Activity = mongoose.model('Activity', activity);
+
+module.exports = Activity;
--- app/processors/RSSProcessor.js
+++ app/processors/RSSProcessor.js
@@ -1,3 +1,5 @@
+const Activity = require('../models/AFactivity');
+
 /*
     unfinshed processor that extracts certain data from the RSS response and returns an array
 */
@@ -6,14 +8,25 @@ function processRSSData (arrayOfRSSItems) {
 
     for (let i = 0; i < arrayOfRSSItems.length; i++) {
         let singleItem = arrayOfRSSItems[i];
+        
+        const activityObject = new Activity({
+            actor: {
+                author: {
+                    displayName: singleItem['dc:creator'][0],
+                }
+            },
+            object: {
+                content: singleItem.description[0],
+                ucdSrcId: singleItem.guid[0]._,
+                ucdEdusModel: {
+                    url: singleItem.link[0],
+                }
+            },
+            title: singleItem.title[0],
+            published: singleItem.pubDate[0].time[0]._, // must be Date format YYYY-MM-DDTHH:mm:ss.SSSZ
+        });
 
-        let object = {
-            title: singleItem.title,
-            link: singleItem.link,
-            description: singleItem.description,
-            guid: singleItem.guid[0]._,
-        }
-        arrayOfActivityObjects.push(object);
+        arrayOfActivityObjects.push(activityObject);
     }
     return arrayOfActivityObjects;
 }
