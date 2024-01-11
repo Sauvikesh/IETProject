6414fa7a3387a7f36b2153775617c790c879f59a
Sauvikesh <81947858+Sauvikesh@users.noreply.github.com>
2024-01-10 17:57:23 -0800

    AF - 105: endpoint now retrieves connector data from AF server

--- app/routes/api.js
+++ app/routes/api.js
@@ -13,16 +13,39 @@ const get20Activities = require('../database/getActivities');
 // route that gets sources
 router.get('/sources', async (req, res) => {
     const apiKey = "b1e07a9f-3d22-4251-aef3-3e1becbd2e51";
-    const apiURL = "http://localhost:8080/api/v1/admin-tool/source";
+    const apiURL = "http://edustream:8080/api/v1/source";
 
-    axios.get(apiURL, {
-        headers : {
-            "Authorization": apiKey
+
+    const settings = {
+        params: {
+            connectorType: "all",
+        },
+        headers: {
+          'Authorization': `ApiKey ${apiKey}`,
         }
-    })
+    }
+
+
+    console.log("about to make the call");
+    axios.get(apiURL, settings)
     .then(async (response) => {
-        res.send(response);
+        res.send(response.data);
     })
+    .catch(error => {
+        // Handle error
+        if (error.response) {
+          // The request was made, but the server responded with an error status
+          console.error(`Status: ${error.response.status}`);
+          console.error(`Data: ${error.response.data}`);
+        } else if (error.request) {
+          // The request was made, but no response was received
+          console.error('No response received');
+        } else {
+          // Something happened in setting up the request that triggered an error
+          console.error(`Error message: ${error.message}`);
+        };
+});
+    
 });
 
 // route that deletes data to be used for testing
