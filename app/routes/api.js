bd362a90862f0c7743445d21f49191924298308c
Sauvikesh <81947858+Sauvikesh@users.noreply.github.com>
2024-01-12 17:53:29 -0800

    AF - 105: moved getRSSConnector function to its own file

--- app/routes/api.js
+++ app/routes/api.js
@@ -8,52 +8,7 @@ const processRSSJSONToAct = require('../processors/RSSJSONToAct');
 const insertActivities = require('../database/insertActivity');
 const clearData = require('../database/deleteAllData');
 const get20Activities = require('../database/getActivities');
-
-
-async function getRSSConnectors() {
-  const apiKey = "b1e07a9f-3d22-4251-aef3-3e1becbd2e51";
-  const apiURL = "http://edustream:8080/api/v1/source";
-
-  const settings = {
-      params: {
-          connectorType: "rss",
-      },
-      headers: {
-        'Authorization': `ApiKey ${apiKey}`,
-      }
-  }
-
-  try {
-      const response = await axios.get(apiURL, settings)
-      const rssConnectors = response.data[0].connectors[0];
-
-      const connectorData = {
-        id: rssConnectors.id,
-        name: rssConnectors.name,
-        uri: rssConnectors.uri,
-        enabled: rssConnectors.enabled,
-        approveBy: rssConnectors.approveBy,
-        includeImage: rssConnectors.includeImages,
-        startSyncWindow: rssConnectors.startSyncWindow,
-        endSyncWindow: rssConnectors.endSyncWindow
-      }
-      return connectorData;
-    } catch(error) {
-      // Handle error
-      if (error.response) {
-        // The request was made, but the server responded with an error status
-        console.error(`Status: ${error.response.status}`);
-        console.error(`Data: ${error.response.data}`);
-      } else if (error.request) {
-        // The request was made, but no response was received
-        console.error('No response received');
-      } else {
-        // Something happened in setting up the request that triggered an error
-        console.error(`Error message: ${error.message}`);
-      };
-      throw error; 
-  };
-}
+const getRSSConnectors = require('../utils/getRSSConnector');
 
 // route that deletes data to be used for testing
 router.get('/clearData', async (req, res) => {
@@ -63,8 +18,9 @@ router.get('/clearData', async (req, res) => {
 router.get('/getRSS', async (req, res) => {
     const connector = await getRSSConnectors();
 
+    const uri = connector.uri;
     console.log(connector);
-    axios.get("https://www.ucdavis.edu/news/latest/rss")
+    axios.get(uri)
     .then(async (response) => {
         // parses xml response and turns it into json
         const jsonData = await processXMLToRSSJSON(response.data);
---
+++ app/utils/getRSSConnector.js
@@ -0,0 +1,46 @@
+async function getRSSConnectors() {
+    const apiKey = "b1e07a9f-3d22-4251-aef3-3e1becbd2e51";
+    const apiURL = "http://edustream:8080/api/v1/source";
+  
+    const settings = {
+        params: {
+            connectorType: "rss",
+        },
+        headers: {
+          'Authorization': `ApiKey ${apiKey}`,
+        }
+    }
+  
+    try {
+        const response = await axios.get(apiURL, settings)
+        const rssConnectors = response.data[0].connectors[0];
+  
+        const connectorData = {
+          id: rssConnectors.id,
+          name: rssConnectors.name,
+          uri: rssConnectors.uri,
+          enabled: rssConnectors.enabled,
+          approveBy: rssConnectors.approveBy,
+          includeImage: rssConnectors.includeImages,
+          startSyncWindow: rssConnectors.startSyncWindow,
+          endSyncWindow: rssConnectors.endSyncWindow
+        }
+        return connectorData;
+      } catch(error) {
+        // Handle error
+        if (error.response) {
+          // The request was made, but the server responded with an error status
+          console.error(`Status: ${error.response.status}`);
+          console.error(`Data: ${error.response.data}`);
+        } else if (error.request) {
+          // The request was made, but no response was received
+          console.error('No response received');
+        } else {
+          // Something happened in setting up the request that triggered an error
+          console.error(`Error message: ${error.message}`);
+        };
+        throw error; 
+    };
+  }
+
+module.exports = getRSSConnectors;
